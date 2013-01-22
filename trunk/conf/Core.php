8daaa2ffb8d496a93b999459c863c4613448d183
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-01-22 06:29:01 +0000

    Change a Default View Dir

--- trunk/conf/Core.php
+++ trunk/conf/Core.php
@@ -3,7 +3,7 @@ namespace conf;
 
 class Core {
 	const ENTRY_SCRIPT = "index.php";
-	const DEFAULT_VIEW_PATH = "app/view/";
+	const DEFAULT_VIEW_PATH = "resources/views/";
 
 	const DEFAULT_CONTROLLER = "IndexController";
 	const CONTROLLER_DEFAULT_METHOD = "index";
