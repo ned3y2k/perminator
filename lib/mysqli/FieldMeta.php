da59b1875aaf0f5b2e20b00b1918ec1302aafa80
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-01-27 07:09:11 +0000

    Add a MySQLI Library

---
+++ lib/mysqli/FieldMeta.php
@@ -0,0 +1,19 @@
+<?php
+namespace lib\mysqli;
+
+class FieldMeta {
+	public $orgName;
+	public $name;
+	public $type;
+	public $orgTable;
+	public $table;
+
+	function __set($name, $value) {
+		if($name == "type") {
+			$explode = explode('(', $value);
+			$this->type = [$explode[0], substr($explode[1], -1)];
+		} else {
+			$this->$name = $value;
+		}
+	}
+}
