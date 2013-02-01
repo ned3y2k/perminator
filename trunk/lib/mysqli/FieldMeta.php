692ab68daedb282a38bd2bb8004e9b8b62b86dd2
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-02-01 18:11:16 +0000

    php 5.3 compatibility fix

--- trunk/lib/mysqli/FieldMeta.php
+++ trunk/lib/mysqli/FieldMeta.php
@@ -11,7 +11,7 @@ class FieldMeta {
 	function __set($name, $value) {
 		if($name == "type") {
 			$explode = explode('(', $value);
-			$this->type = [$explode[0], substr($explode[1], -1)];
+			$this->type = array($explode[0], substr($explode[1], -1));
 		} else {
 			$this->$name = $value;
 		}
