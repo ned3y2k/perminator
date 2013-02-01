76ce54dd574bbd27a34824d8788e0431455c632a
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-02-01 18:33:53 +0000

    move resource directory

--- trunk/app/view/memberShow.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head><title>멤버</title></head>
-<body>
-	id: <?=$this->modelMap["member"]->getId()?><br>
-	pw: <?=$this->modelMap["member"]->getPassword()?><br>
-	name: <?=$this->modelMap["member"]->getName()?><br>
-</body>
-
-</html>
