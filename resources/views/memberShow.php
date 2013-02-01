f0848b0f7150c0820821e34d9f83a3916e6f00b4
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-02-01 18:34:14 +0000

    move resorce directory =

---
+++ resources/views/memberShow.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>멤버</title></head>
+<body>
+	id: <?=$this->modelMap["member"]->getId()?><br>
+	pw: <?=$this->modelMap["member"]->getPassword()?><br>
+	name: <?=$this->modelMap["member"]->getName()?><br>
+</body>
+
+</html>
