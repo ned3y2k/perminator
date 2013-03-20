2bd856a9dd47729613135e61975308ecc4786617
ned3y2k@gmail.com <ned3y2k@gmail.com@26e34c9c-fbf5-9528-7eb7-1044e896ee7a>
2013-03-20 01:25:09 +0000

    Magicquote Environment Can Request Array Mapping

--- classes/web/script/http/Request.php
+++ classes/web/script/http/Request.php
@@ -52,24 +52,27 @@ class Request { // FIXME DataBinder와 연결 하는 작업 필요
 	 */private function processRequest($method) {
 		if (get_magic_quotes_gpc()) {
 			foreach ($method as $key => $value) {
-				$this->parameters[$key] = stripslashes($value);
+				if(is_array($value)) { $this->processRequest($value); $this->parameters[$key] = $value;}
+				else { $this->parameters[$key] = stripslashes($value); }
 			}
 		} else {
 			$this->parameters = $method;
 		}
 	}
 
-	public function getQueryString(array $excludeKeys = null) {
+	public function getQueryString($concatString = "", array $excludeKeys = null) {
 		$query = "";
 
-		if(is_null($excludeKeys)) $excludeKeys = array();
+		if(is_null($excludeKeys)) { $excludeKeys = array(); }
 		$excludeKeys = array_merge(array('class', 'method'), $excludeKeys);
 		foreach ($this->parameters as $key => $value) {
-			if(!in_array($key, $excludeKeys))
+			if(!in_array($key, $excludeKeys)) {
 				$query .= "&{$key}={$value}";
+			}
 		}
 
-		return substr($query, 1);
+		$result = substr($query, 1);
+		return strlen($result) > 0 ? $concatString.$result : "";
 	}
 
 	public function print_r() {
@@ -80,16 +83,12 @@ class Request { // FIXME DataBinder와 연결 하는 작업 필요
 	 * 웹 파라미터를 가져온다.
 	 * 없다면 기본값을 반환
 	 *
-	 * @param String $key
-	 *        	키
-	 * @param String $defaultValue
-	 *        	기본값[optional]
+	 * @param String $key 키
+	 * @param String $defaultValue 기본값[optional]
 	 * @return mixed
 	 */
-	public function getParameter($key, $defaultValue = NULL) {
-		return array_key_exists($key, $this->parameters)
-				&& $this->parameters[$key] != NULL ? $this->parameters[$key]
-				: $defaultValue;
+	public function getParameter($key, $defaultValue = null) {
+		return array_key_exists($key, $this->parameters) && $this->parameters[$key] != null ? $this->parameters[$key] : $defaultValue;
 	}
 
 	/**
