6f549cf27c34a58e2c8700e450849de71093deac
Kyeongdae Moon <ned3y2k@gmail.com>
2018-07-17 16:53:27 +0900

    API 관련 추가

---
+++ src/classes/api/model/StringResult.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyeongdae
+ * Date: 2018-07-17
+ * Time: 오후 4:42
+ */
+
+namespace classes\api\model;
+
+
+class StringResult {
+	/** @var string */
+	private $contentType;
+	/** @var string */
+	private $content;
+
+	/**
+	 * @param string $content
+	 * @param string $contentType text/html
+	 */
+	public function __construct($content, $contentType) {
+		$this->contentType = $contentType;
+		$this->content = $content;
+	}
+
+	/** @return string */
+	public function getContentType() { return $this->contentType; }
+
+	/** @return string */
+	public function getContent() { return $this->content; }
+}
---
+++ src/classes/exception/api/ApiException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyeongdae
+ * Date: 2018-07-17
+ * Time: 오후 4:51
+ */
+
+namespace classes\exception\api;
+
+
+class ApiException {
+
+}
